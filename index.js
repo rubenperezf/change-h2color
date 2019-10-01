c0ddee3e182797d0b15069cdef6349fb6ebe0a37
Ruben Perez <49155114+rubenperezf@users.noreply.github.com>
2019-10-01 10:32:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function change_color() {
+    document.getElementsByTagName("h2")[0].style.color="red";
+}
+function change_color1() {
+    document.getElementById("cute_text").style.color="yellow";
+}
+function change_color2() {
+    document.getElementById("cute_text").style.color="pink";
+}
